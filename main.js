e2330af3916014d9fe1d3c3df214396440410cc3
jeromesenyonga <jeromesenyonga4@gmail.com>
2018-05-02 02:23:34 -0700

    First Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById('button1').onclick = function(){
+    document.getElementById('image').src='Bulb1.svg';
+}
+document.getElementById('button2').onclick= function(){
+    document.getElementById('image').src='Bulb.svg'
+}    
